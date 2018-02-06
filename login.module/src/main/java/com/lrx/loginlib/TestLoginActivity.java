b9bfdd72b30c06fe1b3370f1976bd7ee05e47c27
daven.liu <liurx589060@163.com>
2018-02-06 19:12:45 +0800

    1

---
+++ login.module/src/main/java/com/lrx/loginlib/TestLoginActivity.java
@@ -0,0 +1,34 @@
+package com.lrx.loginlib;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by daven.liu on 2018/2/6 0006.
+ */
+
+public class TestLoginActivity extends Activity {
+    private Button mLoginBtn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        init();
+    }
+
+    private void init() {
+        mLoginBtn = (Button) findViewById(R.id.login_btn);
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginApiImp loginApiImp = new LoginApiImp();
+                loginApiImp.loginByGuest("RTDTER");
+            }
+        });
+    }
+}
